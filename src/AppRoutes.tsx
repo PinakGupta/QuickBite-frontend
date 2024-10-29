973c51ef6771dfca3f1963dbb48224321e1ec9bf
PinakGupta <pinakgupta99@gmail.com>
2024-10-29 09:47:47 +0530

    added search by applying filters functionality

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -4,12 +4,22 @@ import HomePage from "./pages/HomePage";
 import AuthCallbackPage from "./pages/AuthCallbackPage";
 import UserProfilePage from "./pages/UserProfilePage";
 import ProtectedRoute from "./auth/ProtectedRoute";
+import SearchPage from "./pages/SearchPage";
 import ManageRestaurantPage from "./pages/ManageRestaurantPage";
 
 const AppRoutes = ()=>{
     return (
         <Routes>
             <Route path = "/" element ={<Layout showHero><HomePage/></Layout>}/>
+            <Route path="/auth-callback" element={<AuthCallbackPage />} />
+            <Route
+                path="/search/:city"
+                element={
+                <Layout showHero={false}>
+                    <SearchPage />
+                </Layout>
+                }
+            />
             <Route path = "/auth-callback" element ={<AuthCallbackPage/>}/>
             <Route element = {<ProtectedRoute/>}>
                 <Route path = "/user-profile" element ={<Layout><UserProfilePage/></Layout>}/>
